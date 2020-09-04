7256c28b26b63baaede99ba80bb0dcc8c2f6b834
sami.hella.sio@gmail.com <developpeur@SLAM5-HELLA.home>
2020-09-04 15:31:47 +0200

    classes Repas, Repas

---
+++ Repas.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+
+public class Repas {
+	
+	int numero;
+	LocalDate date;
+	
+	public Repas(int numero, LocalDate date) {
+		super();
+		this.numero = numero;
+		this.date = date;
+	}
+	
+
+}
